a44d049d2c0cc86466b48a2a387bf7b18a932058
jenn <epistemicpolymathic5@gmail.com>
2017-03-31 13:19:40 -0400

    login.php complete...  Users can log in now just need association with users table

--- login.php
+++ login.php
@@ -12,83 +12,18 @@
 ?>
 <!DOCTYPE html>
 <html>
-<style>
-    /* Login Page CSS */
-    form {
-        border: 3px solid #f1f1f1;
-    }
-
-    input[type=text], input[type=password] {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-    }
-
-    button {
-        background-color: #4CAF50;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        cursor: pointer;
-        width: 100%;
-    }
-
-    button:hover {
-        opacity: 0.8;
-    }
-
-    .cancelbtn {
-        width: auto;
-        padding: 10px 18px;
-        background-color: #f44336;
-    }
-
-    .imgcontainer {
-        text-align: center;
-        margin: 24px 0 12px 0;
-    }
-
-    img.avatar {
-        width: 40%;
-        border-radius: 50%;
-    }
-
-    .container {
-        padding: 16px;
-    }
-
-    span.psw {
-        float: right;
-        padding-top: 16px;
-    }
-
-    /* Change styles for span and cancel button on extra small screens */
-    @media screen and (max-width: 300px) {
-        span.psw {
-            display: block;
-            float: none;
-        }
-        .cancelbtn {
-            width: 100%;
-        }
-    }
-</style>
 <head>
     <title>My University Schema-Login</title>
-    <link rel="stylesheet" type="text/css" href="/css/login.css"/>
+    <link rel="stylesheet" type="text/css" href="css/login.css"/>
 </head>
 
 <body>
 
 <h2>Login Form</h2>
 
-<form action="login_action.php" method="post">
+<form action="users/login_action.php" method="post">
     <div class="imgcontainer">
-        <img src="/img/unc-charlotte-logo.gif" alt="Avatar" class="avatar">
+        <img src="img/unc-charlotte-logo.gif" alt="UNCC Logo" class="UNCCLogo">
     </div>
 
     <div class="container">
---
+++ users/login_action.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EpistemicPolymath
+ * Date: 3/31/2017
+ * Time: 12:01 PM
+ */
+session_start();
+
+require_once('../db_error/database.php');
