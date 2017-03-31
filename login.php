1b941eb23335b4b21a4799b9fe9be58567a4922b
jenn <epistemicpolymathic5@gmail.com>
2017-03-31 14:38:30 -0400

    login_action.php:
    $_SESSION variables created and error messages created.

--- login.php
+++ login.php
@@ -23,15 +23,15 @@
 
 <form action="users/login_action.php" method="post">
     <div class="imgcontainer">
-        <img src="img/unc-charlotte-logo.gif" alt="UNCC Logo" class="UNCCLogo">
+        <img src="img/unc-charlotte-logo.gif" alt="UNCC Logo">
     </div>
 
     <div class="container">
         <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+        <input type="text" placeholder="Enter Username" name="username" required>
 
         <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="password" placeholder="Enter Password" name="pass" required>
 
         <button type="submit">Login</button>
         <input type="checkbox" checked="checked"> Remember me
@@ -39,7 +39,7 @@
 
     <div class="container" style="background-color:#f1f1f1">
         <button type="button" class="cancelbtn">Cancel</button>
-        <span class="psw">Forgot <a href="#">password?</a></span>
+       <!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
     </div>
 </form>
 
--- users/login_action.php
+++ users/login_action.php
@@ -5,12 +5,49 @@
  * Date: 3/31/2017
  * Time: 12:01 PM
  */
+
+#Start Session
 session_start();
 
+#Require The Database
 require_once('../db_error/database.php');
 
+#Get user's username and password from login.php form
+$username = $_POST['username'];
+$password = $_POST['pass'];
 
-$username = $_POST['uname'];
-$password = $_POST['psw'];
+//Users Table Query
+#Select all users from users table that have the same username and password as the login
+$usersSelectQuery = $db->prepare("SELECT * FROM users
+                                          WHERE userName = :username AND password = :password");
+$usersSelectQuery->execute(array(
+    ":username" => $username,
+    ":password" => $password
+));
+$userSelect = $usersSelectQuery->fetch();
+//$usersSelectQuery->closeCursor();
 
-print_r($_POST);
+#Now we have an array of Users that the Select Query Matched...
+#If the array is > 0, or in other words the Select Query found a match we set the $_SESSIONS
+
+if ($userSelect > 0) {
+# We are adding Session variables for the userName, role, and ID
+    $_SESSION['userName'] = $userSelect['userName'];
+    $_SESSION['userRole'] = $userSelect['role'];
+    $_SESSION['userID'] = $userSelect['userID'];
+
+# Now we need an if statement to check role and determine which homepage to send them to
+    if($userSelect['role'] == 'manager'){
+        header("Location:../manager_driver/index.php");
+    } elseif($userSelect['role'] == 'student'){
+        header("Location:../student_driver/student_home.php");
+    }else{
+        header("Location:../login.php");
+        $errorResponse = "User has unrecognized role.";
+    }
+} else {
+
+    $errorResponse = "Username and Password were not found.";
+}
+
+#In above IF statement two different types of error messages can be set.
